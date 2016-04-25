28fa3bcb44f85d4fc53ad578bb639b9306b972b3
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-04-25 17:22:39 -0400

    added dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [hitchhiker-tree "0.1.0"]
+                 [datascript "0.15.0"]]
   :main ^:skip-aot datacrypt.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/datacrypt/core.clj
+++ src/datacrypt/core.clj
@@ -1,4 +1,8 @@
 (ns datacrypt.core
+  (:require [hitchhiker.redis :as redis]
+            [hitchhiker.tree.core :as core]
+            [hitchhiker.tree.messaging :as msg]
+            [datascript.core :as d])
   (:gen-class))
 
 (defn -main
